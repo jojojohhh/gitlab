3bb2dbdca3cb927e6739bc5cc077a719bbddad21
jojojohhh <qwhtkdwp@gmail.com>
2021-07-12 19:05:57 +0900

    init commit

--- src/main/java/com/swlab/gitlab/GitlabApplication.java
+++ src/main/java/com/swlab/gitlab/GitlabApplication.java
@@ -1,4 +1,11 @@
 package com.swlab.gitlab;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class GitlabApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GitlabApplication.class, args);
+    }
 }
