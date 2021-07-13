63bfe58c941c2beb3461b9d34eed9a4ebab0a7ad
jojojohhh <qwhtkdwp@gmail.com>
2021-07-14 05:58:16 +0900

    logging 설정

--- src/test/java/com/swlab/gitlab/web/ApiControllerTest.java
+++ src/test/java/com/swlab/gitlab/web/ApiControllerTest.java
@@ -2,13 +2,12 @@ package com.swlab.gitlab.web;
 
 import com.swlab.gitlab.config.properties.GitlabProperties;
 
+import lombok.extern.slf4j.Slf4j;
 import org.gitlab4j.api.GitLabApi;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,25 +16,17 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@Slf4j
 public class ApiControllerTest {
 
     @Autowired
-    private GitlabProperties gitlabProperties;
-
-    @Autowired
     private MockMvc mockMvc;
-
-    static final Logger log = LoggerFactory.getLogger(ApiControllerTest.class);
-
-    private GitLabApi gitLabApi;
-
-    public GitLabApi getGitLabApi() {
-        return new GitLabApi(gitlabProperties.getUrl(), gitlabProperties.getPersonalAccessToken());
-    }
 
     @Test
     public void getGitlabVersionTest() throws Exception {
@@ -43,6 +34,7 @@ public class ApiControllerTest {
 
         mockMvc.perform(MockMvcRequestBuilders.get(url)
             .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(result -> {
                     MockHttpServletResponse res = result.getResponse();
                     log.info(res.getContentAsString());
