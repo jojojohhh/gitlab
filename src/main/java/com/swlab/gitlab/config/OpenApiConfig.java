b216a6af00768d12faaf1b14100127efc814036e
jojojohhh <qwhtkdwp@gmail.com>
2021-07-15 11:49:16 +0900

    Open Api, swagger ui 적용

---
+++ src/main/java/com/swlab/gitlab/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.swlab.gitlab.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("API")
+                        .version("1.0")
+                        .description("GitLab API"));
+    }
+
+}
